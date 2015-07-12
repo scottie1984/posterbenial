2f4c4d045714a69da316fb4281c47896997e28c7
obzezzo <prodsfly@hotmail.com>
2015-07-12 17:13:44 -0500

    css
    updating css files

--- public/js/script.js
+++ public/js/script.js
@@ -10,34 +10,32 @@ jQuery(function($) {
     
     $(document).ready( function() {
 
+
         var defaultImgSrc = $('img.main-img').attr('src');
         $.backstretch(defaultImgSrc, {speed: 500});
     
-    $("#m-btn").click(function(){
-        $("#responsive").toggle();
-    });
+    //$("#m-btn").click(function(){
+      //  $("#responsive").toggle();
+    //});
     
-        var mainMenuList = $('#menu-body').html();
-        $('#responsive').html(mainMenuList);
+       // var mainMenuList = $('#menu-body').html();
+       // $('#responsive').html(mainMenuList);
     
-    $("#menu-body a, #responsive a, #upload-body a").on('click',function(e){
+    $("#menu-body a, #upload-body a").on('click',function(e){
             e.preventDefault();
-            $("#errorMsg").hide();
-           $("#successMsg").hide();
-           $("#updateCreate").show();
             if (menu == false)
             {
                 menu = true;
                 
                 var name = $(this).attr('href');
-                $('#menu-body li').removeClass('active');
-                $('#responsive li').removeClass('active');
-                $('#upload-body li').removeClass('active');
+                //$('#menu-body li').removeClass('active');
+                //$('#responsive li').removeClass('active');
+                //$('#upload-body li').removeClass('active');
 
                 var menuClass = $(this).parent('li').attr('class');
-                $('.'+menuClass).addClass('active');
+                //$('.'+menuClass).addClass('active');
 
-                $("#responsive").hide();
+                //$("#responsive").hide();
 
                 var imgSrc = $("img"+name+"-img").attr('src');
                 $.backstretch(imgSrc, {speed: 500}); 
